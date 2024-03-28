4725866b452c032d68b66fb14889a4180aaccbe3
Angel09122002 <gabrielquintero1221@gmail.com>
2024-03-28 15:41:43 -0400

    cart-modal logic added

--- main.js
+++ main.js
@@ -1,35 +1,55 @@
 // Changes of quantity assign to the input, add or subtract items from the cart
 let minusBtn = document.querySelector(".input__minus");
 let plusBtn = document.querySelector(".input__plus");
-let userInput = document.querySelector(".input__number")
+let userInput = document.querySelector(".input__number");
 let userInputNumber = 0;
 
 //after the listener, it will run a arrow function that was added as a parameter
-plusBtn.addEventListener("click", ()=>{
-    userInputNumber ++; 
-    userInput.value = userInputNumber;
-    console.log(userInputNumber)
-}); 
+plusBtn.addEventListener("click", () => {
+  userInputNumber++;
+  userInput.value = userInputNumber;
+  console.log(userInputNumber);
+});
 
-minusBtn.addEventListener('click', ()=>{
-    
-   userInputNumber --;
-//Make sure that the userInputNumber never goes below zero
-   if (userInputNumber <= 0) {
-        userInputNumber = 0;
-   }
-   userInput.value = userInputNumber;
-    console.log(userInputNumber )
+minusBtn.addEventListener("click", () => {
+  userInputNumber--;
+  //Make sure that the userInputNumber never goes below zero
+  if (userInputNumber <= 0) {
+    userInputNumber = 0;
+  }
+  userInput.value = userInputNumber;
+  console.log(userInputNumber);
 });
 // Add the total od the products into the cart when the "add to cart" is press
 
-const addToCartBtn = document.querySelector('.details__button');
+const addToCartBtn = document.querySelector(".details__button");
 // The cartNotification variable will be changing according to the user input
-let cartNotification = document.querySelector('header__cart--notification');
-  
-//Call the addToCartBtn and then add a listener that will execute an arrow function
+let cartNotification = document.querySelector(".header__cart--notification");
 
-addToCartBtn.addEventListener('click', ()=>{
-    cartNotification.innerText = userInputNumber;
-    console.log(cartNotification);
+//Call the addToCartBtn and then add a listener that will execute an arrow function
+addToCartBtn.addEventListener("click", () => {
+  // last value will take the initial value from cartNotification, transform it in a number using parseInt
+  let lastValue = parseInt(cartNotification.innerText);
+  // Call the lastValue and add the input from the user (userInputNumber)
+  lastValue = lastValue + userInputNumber;
+
+  //Call the cartNotification variable and then use the property .innerText
+  //insert the value of lastValue in the cartNotification variable
+  cartNotification.innerText = lastValue;
+  //The "header__cart--notification" has a display:none. Call the variable that contains the element and change the styles.
+  cartNotification.style.display = "block";
+  console.log(cartNotification);
 });
+
+// Gallery modal logic
+
+const CartIconBtn = document.querySelector(".header__cart");
+const cartModal = document.querySelector(".cart-modal");
+
+CartIconBtn.addEventListener("click", () => {
+  //classList will return a list of the scss classes applied to the element
+  // toggle alternate the class adding the new 'show' class
+  cartModal.classList.toggle("show");
+});
+
+let priceModal = document.querySelector(".cart-modal__price");
